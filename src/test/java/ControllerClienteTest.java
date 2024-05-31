08fc946f8f3f12b7b2897bee469579bc5bec8767
Ian <100783092+IanRRocha@users.noreply.github.com>
2024-05-31 16:02:30 -0300

    Update ControllerClienteTest.java

--- src/test/java/ControllerClienteTest.java
+++ src/test/java/ControllerClienteTest.java
@@ -31,73 +31,63 @@ public class ControllerClienteTest {
 
     @Test
     public void testGetClientes() {
-        // Arrange
+        
         ArrayList<Cliente> clientes = new ArrayList<>();
         clientes.add(new ClientePF(1, new Date(), "Cliente 1", "12345678901", 12345));
         clientes.add(new ClientePJ(2, new Date(), "Cliente 2", "12345678901234", 54321));
         when(mockDAOCliente.getAll()).thenReturn(clientes);
         
-        // Act
         ArrayList<Cliente> result = controller.getClientes();
         
-        // Assert
         assertNotNull(result);
         assertEquals(2, result.size());
     }
 
     @Test
     public void testGetCliente() {
-        // Arrange
+        
         ClientePF cliente = new ClientePF(1, new Date(), "Cliente 1", "12345678901", 12345);
         when(mockDAOCliente.getCliente(1)).thenReturn(cliente);
 
-        // Act
         Cliente result = controller.getCliente(1);
 
-        // Assert
         assertNotNull(result);
         assertEquals(cliente, result);
     }
 
     @Test
     public void testPersistirBancoInsert() {
-        // Arrange
+
         ClientePF cliente = new ClientePF(1, new Date(), "Cliente 1", "12345678901", 12345);
         
-        // Act
         controller.persistirBanco(cliente, true, false, false);
         
-        // Assert
         verify(mockDAOCliente, times(1)).insert(cliente);
     }
 
     @Test
     public void testPersistirBancoEdit() {
-        // Arrange
+
         ClientePF cliente = new ClientePF(1, new Date(), "Cliente 1", "12345678901", 12345);
         
-        // Act
         controller.persistirBanco(cliente, false, true, false);
         
-        // Assert
         verify(mockDAOCliente, times(1)).update(cliente);
     }
 
     @Test
     public void testPersistirBancoDelete() {
-        // Arrange
+
         ClientePF cliente = new ClientePF(1, new Date(), "Cliente 1", "12345678901", 12345);
         
-        // Act
         controller.persistirBanco(cliente, false, false, true);
         
-        // Assert
         verify(mockDAOCliente, times(1)).delete(cliente);
     }
 
     @Test
     public void testToString() {
-        // Arrange
+
         ArrayList<Cliente> clientes = new ArrayList<>();
         clientes.add(new ClientePF(1, new Date(), "Cliente 1", "12345678901", 12345));
         clientes.add(new ClientePJ(2, new Date(), "Cliente 2", "12345678901234", 54321));
@@ -106,10 +96,8 @@ public class ControllerClienteTest {
         String expected = "Cliente{id=1, data=" + new Date() + ", nome='Cliente 1', cpf='12345678901', identidade=12345}\n" +
                           "Cliente{id=2, data=" + new Date() + ", nome='Cliente 2', cnpj='12345678901234', inscricaoEstadual=54321}\n";
 
-        // Act
         String result = controller.toString();
 
-        // Assert
         assertEquals(expected, result);
     }
 }
