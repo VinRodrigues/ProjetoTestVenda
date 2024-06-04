8dfe737c62e594a8d30108136b9d4b55c4c6e399
Vinicius Moura Rodrigues <99925877+VinRodrigues@users.noreply.github.com>
2024-06-04 14:29:13 -0300

    teste 2 ControllerProdutoTest.java

--- test/ControllerProdutoTest.java
+++ test/ControllerProdutoTest.java
@@ -4,8 +4,9 @@ import org.junit.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+/*
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
+*/
 /**
  *
  * @author ian21
