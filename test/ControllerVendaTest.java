a6b5a2bcb3b79d9a23b3df425d2253c3121688f0
Ian <100783092+IanRRocha@users.noreply.github.com>
2024-06-04 17:42:11 -0300

    Add import Date

--- test/ControllerVendaTest.java
+++ test/ControllerVendaTest.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import org.junit.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
