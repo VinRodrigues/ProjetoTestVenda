d7a733d389f23530cbdd151609bbe446bf3a170a
Vinicius Moura Rodrigues <99925877+VinRodrigues@users.noreply.github.com>
2024-06-04 17:26:46 -0300

    Update ControllerVendaTest.java

--- test/ControllerVendaTest.java
+++ test/ControllerVendaTest.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import org.junit.*;
+import main.java.Controller.ControllerCliente;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
